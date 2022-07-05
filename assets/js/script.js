c81ddac618d12dd05d1a538bf1a36cbd83c8c251
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-07-05 14:56:03 -0400

    final product before final refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,59 +3,54 @@ window.alert("Welcome to my password generator! Please follow the prompts to gen
 
 
 
-var passwordLength = charcount;
+// var passwordLength = charcount;
 
-var upperCase = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-var lowerCase = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-var specialCharacters =[",!,@,#,$,%,^,&,*,(,),"];
-var numbers = ["1,2,3,4,5,6,7,8,9,0"];
-
-
+var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+var specialCharactersChars = "!@#$%^&*()";
+var numberChars = "1234567890";
 
 
-function howManyChars(){
-  var charcount= prompt("How many character would you like in your password?  Please select a count between 8-128. ");
+
+function generatePassword(){
+  var passwordReturned = ""
+  var chars = ""
+  var charcount = prompt("How many character would you like in your password?  Please select a count between 8-128. ");
    
    if(charcount > 130 || charcount < 8 ){
-   window.alert("invalid response")}
+   return window.alert("invalid response")}
  
-   else(charcount < 128 || charcount > 8 ) 
-     { var upperCase = window.prompt("Would you like Uppercase characters in your password? Type 1 for yes, type 0 for no.")
-     var lowerCase = window.prompt("Would you like Lowercase characters in your password?  Type 1 for yes, type 0 for no. ")
-    var numbers = window.prompt("Would you like numeric characters in your password? Type 1 for yes, type 0 for no.")
-     var specialCharacter = window.prompt("Would you like special characters in your password? Type 1 for yes, type 0 for no.")
+   if(charcount < 128 || charcount > 8 ) 
+     { var upperCase = window.confirm("Would you like Uppercase characters in your password? Type 1 for yes, type 0 for no.")
+     var lowerCase = window.confirm("Would you like Lowercase characters in your password?  Type 1 for yes, type 0 for no. ")
+    var numbers = window.confirm("Would you like numeric characters in your password? Type 1 for yes, type 0 for no.")
+     var specialCharacter = window.confirm("Would you like special characters in your password? Type 1 for yes, type 0 for no.")
 
    }
+   if(upperCase) {
+    chars = chars + upperCaseChars
+  }
+
+
+  if(lowerCase) {
+    chars = chars + lowerCaseChars
+  }
+  if(numbers) {
+    chars = chars + numberChars
+  }
+  if(specialCharacter) {
+    chars = chars + specialCharactersChars
+  }
+  console.log(chars)
+
+  for (let i = 0; i < charcount; i++) {
+    passwordReturned = passwordReturned + chars.charAt(Math.floor(Math.random()*chars.length))
+    
+  }
+ return passwordReturned
   }
     
      
-
-var upperCase = function(){
-
-  if(upperAnswer > 0) {
-    
-  }}
-
-  
-
-var lowerCase = function(){
-
-  Math.floor(Math.random()*10)+1
-}
-
-
-
-var numbers = function(){
-  
-  Math.floor(Math.random()*10)+1
-}
-
-
-
-var specialCharacters = function(){
-  
-  Math.floor(Math.random()*10)+1
-}
 
 
 
